43ff435db4edde51c3c2c7bdc3782e0a9025a0a4
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2024-12-11 14:38:59 +0100

    fix for old python version

--- converter.py
+++ converter.py
@@ -277,7 +277,7 @@ def on_message(client, userdata, msg):
             print("lbdevice_name")
             print(lora_payload)
             lb_id = lora_payload[0]
-            ieee_id = "0x{:016x}".format(int.from_bytes(lora_payload[1:9]))
+            ieee_id = "0x{:016x}".format(int.from_bytes(lora_payload[1:9], "big"))
             name = lora_payload[9:]
             print(str(lb_id))
             print(ieee_id)
